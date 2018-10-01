c052860db5d3a79eea8ec9d188741837ff34603c
HJ SUN <abelsun@sina.com>
2018-10-01 08:24:03 +0800

    Finish data getting in comment list page

--- client/pages/list/list.js
+++ client/pages/list/list.js
@@ -1,18 +1,22 @@
 // pages/list/list.js
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    comments:null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.setData({
+      comments: app.comments
+    })
   },
 
   /**
--- server/controllers/indexFilm.js
+++ server/controllers/indexFilm.js
@@ -9,7 +9,7 @@ module.exports = {
     let data1 = (await DB.query('select * from movies where id = ?',[numb]))[0]
 
     //获取此电影评论
-    let data2 = await DB.query('select * from comments where movie_id = ?',[numb])
+    let data2 = await DB.query('select * from comments where movie_id = ? order by time desc limit 15',[numb])
 
     //合并结果
     ctx.state.data = [data1,data2]
